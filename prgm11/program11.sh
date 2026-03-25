d1739e6df6012650f40fd12dde0477eb75bd1baa
adithyanmkohm-web <adithyanmkohm@gmail.com>
2026-03-25 11:55:22 +0530

    Added my network programs

---
+++ prgm11/program11.sh
@@ -0,0 +1,4 @@
+read -p "Enter the height of the triangle:" h
+read -p "Enter the bredth of the triangle:" b
+area=$(((b*h)/2))
+echo "area of the triangle is:" $area
---
+++ prgm14/program14.sh
@@ -0,0 +1,8 @@
+read -p "enter a three digit number : " n
+while [ $n -ne 0 ]
+do
+        reverse=$reverse$((n%10))
+        n=$((n/10))
+done
+echo "reverse of the number is:"$reverse
+
